be16eea513e76ca325d9d24e4ee4acdb70e06f62
shankar19021998@gmail.com <shankar19021998@gmail.com>
2023-01-06 19:45:09 +0530

    tailwind-basic-setup

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="text-3xl font-bold">
+        Edit <code>src/App.js</code> and save to reload.
+    </h1>
+       
         <a
           className="App-link"
           href="https://reactjs.org"
