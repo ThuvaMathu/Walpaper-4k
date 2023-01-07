7e188711d9a13855fba0731471f88ed324e2b7dd
Thuvamathu <thuvamathu618@gmail.com>
2023-01-08 01:25:21 +1000

    Add first ui

--- client/src/components/landing.js
+++ client/src/components/landing.js
@@ -1,5 +1,11 @@
 import React from "react";
-
+import logo from "../assets/logo-color.png";
+import MainNav from "./navbar/nav";
 export default function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div>
+      {/* <img src={logo} style={{ width: "100%" }} /> */}
+      <MainNav />
+    </div>
+  );
 }
---
+++ client/src/services/dummy-carousal.js
@@ -0,0 +1,14 @@
+export const caraousalData = [
+  "data1",
+  "data2",
+  "data3",
+  "data4",
+  "data5",
+  "data6",
+  "data7",
+  "data8",
+  "data9",
+  "data10",
+  "data11",
+  "data12",
+];
