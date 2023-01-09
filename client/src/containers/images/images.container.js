3fe1deadfa1caebe5f376c7f4ab505d7d3a56743
shankar19021998@gmail.com <shankar19021998@gmail.com>
2023-01-09 22:21:47 +0530

    imageslist

--- client/src/containers/images/images.container.js
+++ client/src/containers/images/images.container.js
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from 'react'
+import { ImageComponents } from '../../components/images/images.componets'
 import { demoResult
  } from '../../services/demo-results'
 
@@ -16,28 +17,7 @@ export const ImagesConatainers = () => {
    
   return (
     <div>
-<div className="container mx-auto">
-<h1 className='text-5xl font-bold m-2'>Desktop Wallpaper</h1>
-<div className="gap-4 columns-1  md:columns-3">
-{images?.map((imagedata, index)=>(
-    <>
-<figure className="relative cursor-pointer">
-    
-    <a>
-        <div className='px-2 py-4'>
-    <img className="w-[480px] h-auto rounded-[5px]"  src={imagedata?.src?.large} />
-    </div>
-    </a>
-    <figcaption class="absolute px-4 text-lg text-white bottom-6">
-      <p className='text-start'>{imagedata.photographer}</p>
-  </figcaption>
-    </figure>
-    </>
-))}
-
-</div>
-
-</div>
+<ImageComponents imagesList={images} title={'Desktop Wallpaper'} />
 
     </div>
   )
