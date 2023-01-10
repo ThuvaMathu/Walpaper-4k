aaca13f44c581d7ad54dad0a036d5bd3756d387a
Lass-wulf <100084147+Lass-wulf@users.noreply.github.com>
2023-01-10 20:17:50 +0530

    carousel

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Carousel from "./carousel/carousel";
 import Landing from "./landing";
 
 export default function Main() {
   return (
     <div>
       <Landing />
+      <Carousel/>
     </div>
   );
 }
