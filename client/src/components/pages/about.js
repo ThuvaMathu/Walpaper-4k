5a3818aff2348bba90599674fee4dc705078cdfe
Thuvamathu <thuvamathu618@gmail.com>
2023-01-11 01:53:21 +1000

    add router with issue

---
+++ client/src/components/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className=" h-96 w-full bg-blue-400 justify-center items-center ">
+      <p className=" text-slate-600 font-bold text-7xl ">About</p>
+    </div>
+  );
+}
