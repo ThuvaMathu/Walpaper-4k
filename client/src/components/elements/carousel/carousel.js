2b249a9eb3d0f3d16da8ba030f95044f59ea364b
Lass-wulf <100084147+Lass-wulf@users.noreply.github.com>
2023-01-12 22:57:02 +0530

    changes

--- client/src/components/elements/carousel/carousel.js
+++ client/src/components/elements/carousel/carousel.js
@@ -1,19 +1,11 @@
-import React from "react";
-import { Splide, SplideTrack, SplideSlide } from "@splidejs/react-splide";
-import "../carousel/carousel.css";
+import React from 'react'
+import { Splide, SplideTrack, SplideSlide, } from '@splidejs/react-splide';
+import '../carousel/carousel.css';
 
 import "@splidejs/splide/css";
 
 export default function Carousel() {
-  const info = [
-    "Slide1",
-    "slide2",
-    "slide2",
-    "slide3",
-    "slide4",
-    "slide5",
-    "slide6",
-  ];
+  const info = ["Slide1", "slide2", "slide2","slide3" ,"slide4" ,"slide5","slide6",   ]
   return (
     <div>
       {/* <div className="container mx-auto px-4 ">
@@ -22,8 +14,13 @@ export default function Carousel() {
             fixedWidth: "300px",
             perPage: "4",
             type: "loop",
-            objectfit: "cover",
-          }}
+        objectfit: "cover",  
+      }}
+         
+          
+         
+        
+        
         >
           {info.map((data) => (
             <SplideSlide>{data}</SplideSlide>
@@ -68,3 +65,17 @@ export default function Carousel() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
