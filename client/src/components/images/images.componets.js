700fa8081d29e08af5b9551326976bcf9b15c16d
shankar19021998@gmail.com <shankar19021998@gmail.com>
2023-01-13 22:02:52 +0530

    download image and size

--- client/src/components/images/images.componets.js
+++ client/src/components/images/images.componets.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import {
   Dialog,
   DialogContent,
@@ -6,13 +6,28 @@ import {
   DialogContentText,
 } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import CloseIcon from "@mui/icons-material/Close";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import Button from '@mui/material/Button';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 export const ImageComponents = ({ imagesList, title }) => {
-  const [open, setOpen] = React.useState(false);
-  const [data, setData] = React.useState();
-  const [fullWidth, setFullWidth] = React.useState(true);
-  const [maxWidth, setMaxWidth] = React.useState("lg");
+  const [open, setOpen] = useState(false);
+  const [data, setData] = useState();
+  const [fullWidth, setFullWidth] = useState(true);
+  const [maxWidth, setMaxWidth] = useState("lg");
   const handleClickOpen = (dataValue) => {
     setData(dataValue);
     setOpen(true);
@@ -21,8 +36,41 @@ export const ImageComponents = ({ imagesList, title }) => {
   const handleClose = () => {
     setOpen(false);
   };
-
-  console.log(data, "data");
+const handleDownload=(image)=>{
+  const blob1 = new Blob([image?.src?.original],{type:'multipart/form-data'})
+  const link= document.createElement('a')
+  link.href = window.URL.createObjectURL(blob1)
+  const filename = image?.src?.original
+  link.download = filename
+  link.click()
+}
+const [selectedsize,setSelectedSize] = useState()
+const [anchorEl, setAnchorEl] = useState(null);
+const [openMenu,setOpenMenu] = useState(false);
+const handleMenuClick = (event) => {
+  setOpenMenu(true)
+  setAnchorEl(event.currentTarget);
+ 
+}
+const handleMenuClose = () => {
+  setAnchorEl(null);
+  setOpenMenu(false)
+};
+const handleChange =(event)=>{
+  setSelectedSize(event.target.value)
+}
+useEffect(() => {
+ 
+}, [openMenu])
+const handleDownloadSlelectedSize=()=>{
+  const blob1 = new Blob([selectedsize],{type:'multipart/form-data'})
+  const link= document.createElement('a')
+  link.href = window.URL.createObjectURL(blob1)
+  const filename = selectedsize
+  link.download = filename
+  link.click()
+  handleMenuClose()
+}
   return (
     <div className="container mx-auto">
       <h1 className="text-5xl font-bold m-2">{title}</h1>
@@ -30,7 +78,7 @@ export const ImageComponents = ({ imagesList, title }) => {
         {imagesList?.map((imagedata, index) => (
           <div
             className="relative  group"
-            onClick={() => handleClickOpen(imagedata)}
+            
           >
             {/* <div className='absolute cursor-pointer  px-4 text-end text-lg text-white top-6 right-2  opacity-0 group-hover:opacity-100'>
         
@@ -44,7 +92,7 @@ export const ImageComponents = ({ imagesList, title }) => {
         </button>
         </div> */}
 
-            <a>
+            <a onClick={() => handleClickOpen(imagedata)}>
               <div className="px-2 py-4">
                 <img
                   className="w-[480px] h-auto rounded-[5px]"
@@ -59,6 +107,7 @@ export const ImageComponents = ({ imagesList, title }) => {
               <button
                 type="button"
                 class="bg-white text-black border border-white-700   font-medium rounded-lg text-sm p-2.5 text-center"
+                onClick={()=>handleDownload(imagedata)}
               >
                 <svg
                   width="24"
@@ -106,19 +155,86 @@ export const ImageComponents = ({ imagesList, title }) => {
         open={open}
         onClose={handleClose}
       >
+        
         <DialogTitle>
-          <h5 className="text-2xl font-bold m-2">{data?.photographer}</h5>
+          <h5 className="text-2xl font-bold m-2">{data?.photographer}
+          
+          
+          </h5>
+          <IconButton
+          aria-label="close"
+         
+          sx={{
+            position: 'absolute',
+            right: 20,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <Button
+       
+        variant="contained"
+        onClick={handleMenuClick}
+       
+      >
+        Free Download | {openMenu === true ? (
+            <ExpandLessIcon />
+          ) : (
+            <ExpandMoreIcon/>
+          )}
+        </Button>
+        <Menu
+        anchorEl={anchorEl}
+       
+        open={openMenu}
+        onClose={handleMenuClose}
+       
+       
+      >
+        <MenuItem>
+        <RadioGroup
+    aria-labelledby="demo-radio-buttons-group-label"
+    name="radio-buttons-group"
+    onChange={(value)=>handleChange(value)}
+  >
+    <FormLabel id="demo-radio-buttons-group-label">Choose a size : </FormLabel>
+    <FormControlLabel value={data?.src?.original} control={<Radio />} label="Original  4480x6720"  />
+    <Divider />
+    <FormControlLabel value={data?.src?.large} control={<Radio />} label="Large  1920x2880" />
+    <Divider />
+    <FormControlLabel value={data?.src?.medium} control={<Radio />} label="Medium  1280x1920" />
+    <Divider />
+    <FormControlLabel value={data?.src?.small} control={<Radio />} label="Small  640x960" />
+    <Divider />
+    <Button
+       
+       variant="contained"
+       onClick={()=>handleDownloadSlelectedSize()}
+      
+     >
+      Download  Size
+      </Button>
+  </RadioGroup>
+         
+        </MenuItem>
+       
+        
+      </Menu>
+        </IconButton>
           <IconButton
             aria-label="close"
             sx={{
-              position: "absolute",
-              right: 8,
-              top: 8,
-              color: (theme) => theme.palette.grey[500],
+              position: "fixed",
+              right:'auto',
+              left: '80px',
+              top: '50px',
+              color: "white",
+          
             }}
           >
-            <CloseIcon onClick={handleClose} />
+            <CloseIcon  onClick={handleClose} />
           </IconButton>
+         
         </DialogTitle>
         <DialogContent
           sx={{ display: "flex", justifyContent: "center !important" }}
@@ -127,7 +243,7 @@ export const ImageComponents = ({ imagesList, title }) => {
             <div className="container mx-auto">
               <img
                 src={data?.src?.large}
-                className=" w-[480px] h-auto justify-center place-content-center"
+                className=" max-h-[75vh] min-h-[300px] max-w-[calc(50vh)] min-w-[calc(200px)] justify-center place-content-center"
               />
             </div>
           </DialogContentText>
