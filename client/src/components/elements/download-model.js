8cd1537c61f1dc739d0d1efe15b2b1c42394db8c
shankar19021998@gmail.com <shankar19021998@gmail.com>
2023-01-16 09:30:22 +0530

    Image download

--- client/src/components/elements/download-model.js
+++ client/src/components/elements/download-model.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import CloseIcon from "../icons/close-icon";
 import ImgSkelton from "../icons/img-skelton";
 import logo from "../../assets/logo-no-bg.png";
+import { Button } from "@mui/material";
 
 export default function DownloadModel(props) {
+  const [selectedsize, setSelectedSize] = useState();
+  const handleChange = (event) => {
+    setSelectedSize(event.target.value);
+  };
+
+  const handleDownloadSlelectedSize = () => {
+    const blob1 = new Blob([selectedsize], { type: "multipart/form-data" });
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob1);
+    const filename = selectedsize;
+    link.download = filename;
+    link.click();
+  
+  };
   return (
     <>
       <div id="popup" className="z-50 fixed w-full flex justify-center inset-0">
@@ -47,7 +62,33 @@ export default function DownloadModel(props) {
                     </div>
                   </div>
                   <div className="w-full lg:w-1/4 h-24 dark:border-gray-700 lg:h-64 border-t lg:border-t-0 lg:border-r lg:border-l lg:rounded-r dark:bg-gray-700 bg-gray-100">
-                    <h5>Download option goes here</h5>
+                    <h5 class="text-md font-bold m-2">Download option goes here</h5>
+
+                    <div class="flex items-center m-3">
+    <input checked id="default-radio-1" type="radio" value={props.imageData?.src?.original} name="default-radio" class="w-4 h-4 bg-gray-100 border-gray-300"  onChange={(value)=>handleChange(value)}></input>
+    <label for="default-radio-1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Original  <span className="text-gray-500"> 4480 x 6720 </span></label>
+</div>
+<hr class="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+<div class="flex items-center m-3">
+    <input  id="default-radio-2" type="radio" value={props.imageData?.src?.large} name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300" onChange={(value)=>handleChange(value)}></input>
+    <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Large  <span className="text-gray-500"> 1920 x 2880 </span></label>
+</div>
+<hr class="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+<div class="flex items-center m-3">
+    <input  id="default-radio-3" type="radio" value={props.imageData?.src?.medium} name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300" onChange={(value)=>handleChange(value)}></input>
+    <label for="default-radio-3" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Medium  <span className="text-gray-500"> 1280 x 1920 </span></label>
+</div>
+<hr class="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+<div class="flex items-center m-3">
+    <input  id="default-radio-4" type="radio"value={props.imageData?.src?.small} name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300" onChange={(value)=>handleChange(value)}></input>
+    <label for="default-radio-4" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Small  <span className="text-gray-500"> 640 x 960 </span></label>
+</div>
+
+<div class="flex items-center m-3">
+<button type="button" onClick={()=>handleDownloadSlelectedSize()} class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Download a Selected Size</button>
+
+     
+      </div>
                   </div>
                 </div>
                 <div className="flex items-center justify-between mt-3">
